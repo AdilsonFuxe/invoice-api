177484d567f94ec2b598cfa7f3ba4aba6bda6e7d
Adilson Fuxe <adilsonfuxe.fuxe@gmail.com>
2023-08-20 22:29:15 +0100

    refactor: move mocked created invoice params to test-helper

--- src/core/use-cases/create-invoice.spec.ts
+++ src/core/use-cases/create-invoice.spec.ts
@@ -1,31 +1,14 @@
 import {createInvoice} from "@src/core/use-cases";
-import {mockedInvoice} from "@test-helper/types";
+import {mockedInvoice, mockedCreateInvoiceParams} from "@test-helper/types";
 
 const makeSut = () => {
   const createInvoiceRepository = jest.fn().mockResolvedValue(mockedInvoice());
-  const mockedParams = () => ({
-    reference: 10,
-    accountId: 'any_account_id',
-    jobs: [{
-      description: 'any_description',
-      price: 1500.00
-    }],
-    bankInfo: {
-      bic: 'any_bic',
-      iban: 'any_iban',
-      fullName: 'any_name'
-    },
-    companyInfo: 'any_company_info',
-    companyName: 'any_company_name',
-    expiresIn: new Date(),
-    generatedAt: new Date()
-  })
 
   const sut = createInvoice({createInvoiceRepository})
 
   return {
     sut,
-    mockedParams,
+    mockedParams: mockedCreateInvoiceParams,
     createInvoiceRepository,
     mockedInvoice
   }
--- test-helper/types/index.ts
+++ test-helper/types/index.ts
@@ -1,4 +1,5 @@
 import {Invoice} from "@src/core/types";
+import {CreateInvoice} from "@src/core/use-cases";
 
 export const mockedInvoice = (): Invoice => ({
   id: 'any_id',
@@ -19,4 +20,22 @@ export const mockedInvoice = (): Invoice => ({
   generatedAt: new Date(),
   createdAt: new Date(),
   updatedAt: new Date()
+})
+
+export const mockedCreateInvoiceParams = ():CreateInvoice.Params => ({
+  reference: 10,
+  accountId: 'any_account_id',
+  jobs: [{
+    description: 'any_description',
+    price: 1500.00
+  }],
+  bankInfo: {
+    bic: 'any_bic',
+    iban: 'any_iban',
+    fullName: 'any_name'
+  },
+  companyInfo: 'any_company_info',
+  companyName: 'any_company_name',
+  expiresIn: new Date(),
+  generatedAt: new Date()
 })
