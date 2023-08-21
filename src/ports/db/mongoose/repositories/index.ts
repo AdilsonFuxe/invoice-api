b46e3ef0eeea6b83e1e7ef993ba404a2afaac5e1
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2023-08-21 09:44:42 +0100

    feat: add load invoice by id repository

--- src/ports/db/mongoose/repositories/index.ts
+++ src/ports/db/mongoose/repositories/index.ts
@@ -1,7 +1,6 @@
-import {CreateInvoice, DeleteInvoiceById, loadAllInvoices} from '@src/core/use-cases';
+import {CreateInvoice, DeleteInvoiceById, LoadAllInvoices, LoadInvoiceById} from '@src/core/use-cases';
 import {InvoiceModel} from "@src/ports/db/mongoose/model";
 import {MongoHelper} from "@src/ports/db/mongoose/helper";
-
 
 export const createInvoiceRepository: CreateInvoice = async (params) => {
   const doc = new InvoiceModel(params);
@@ -13,7 +12,12 @@ export const deleteInvoiceByIdRepository: DeleteInvoiceById = async (id) => {
   await InvoiceModel.findByIdAndDelete(id);
 }
 
-export const loadAllInvoicesRepository = async (accountId) => {
+export const loadAllInvoicesRepository: LoadAllInvoices = async (accountId) => {
   const result = await InvoiceModel.find({accountId});
   return result.map(MongoHelper.serialize);
+}
+
+export const loadInvoiceByIdRepository: LoadInvoiceById = async (id) => {
+  const result = await InvoiceModel.findById(id);
+  return MongoHelper.serialize(result);
 }
