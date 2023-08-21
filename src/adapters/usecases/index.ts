a6886489730390b5f60c0831e0ac5eafa035a89e
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2023-08-21 09:46:05 +0100

    feat: add load invoice by id adapter

--- src/adapters/usecases/index.ts
+++ src/adapters/usecases/index.ts
@@ -1,10 +1,12 @@
-import {createInvoice, deleteInvoiceById, loadAllInvoices} from "@src/core/use-cases";
+import {createInvoice, deleteInvoiceById, loadAllInvoices, loadInvoiceById} from "@src/core/use-cases";
 import {
   createInvoiceRepository,
   deleteInvoiceByIdRepository,
-  loadAllInvoicesRepository
+  loadAllInvoicesRepository,
+  loadInvoiceByIdRepository
 } from "@src/ports/db/mongoose/repositories";
 
 export const createInvoiceAdapter = (params) => createInvoice({createInvoiceRepository})(params);
 export const deleteInvoiceByIdAdapter = (id) => deleteInvoiceById({deleteInvoiceByIdRepository})(id);
 export const loadAllInvoicesAdapter = (accountId) => loadAllInvoices({loadAllInvoicesRepository})(accountId);
+export const loadInvoiceByIdAdapter = (id) => loadInvoiceById({loadInvoiceByIdRepository})(id);
