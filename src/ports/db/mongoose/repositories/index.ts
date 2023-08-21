bdec04116dd58b1bbd5f92ad570ed3b3ad9c2beb
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2023-08-21 09:28:48 +0100

    feat: add delete invoice by id repository

--- src/ports/db/mongoose/repositories/index.ts
+++ src/ports/db/mongoose/repositories/index.ts
@@ -1,4 +1,4 @@
-import {CreateInvoice} from '@src/core/use-cases';
+import {CreateInvoice, DeleteInvoiceById} from '@src/core/use-cases';
 import {InvoiceModel} from "@src/ports/db/mongoose/model";
 import {MongoHelper} from "@src/ports/db/mongoose/helper";
 
@@ -7,3 +7,8 @@ export const createInvoiceRepository: CreateInvoice = async (params) => {
   const doc = new InvoiceModel(params);
   await doc.save();
   return MongoHelper.serialize(doc);
+}
+
+export const deleteInvoiceByIdRepository: DeleteInvoiceById = async (id) => {
+  await InvoiceModel.findByIdAndDelete(id);
+}
